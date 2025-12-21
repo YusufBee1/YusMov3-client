ef3b3242de7dad31f4984abc35241140636a875f
YusufBee1 <152460143+YusufBee1@users.noreply.github.com>
2025-12-21 13:10:50 -0500

    Fix router structure and valid component paths

--- src/index.jsx
+++ src/index.jsx
@@ -1,18 +1,22 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { createRoot } from 'react-dom/client';
 import { MainView } from "./components/main-view/main-view";
+import Container from 'react-bootstrap/Container';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+// Import statement to indicate that you need to bundle `./index.scss`
 import "./index.scss";
 
-const App = () => (
-  <BrowserRouter>
-    <MainView />
-  </BrowserRouter>
-);
+// Main component (will eventually use all the others)
+const App = () => {
+  return (
+    <Container>
+      <MainView />
+    </Container>
+  );
+};
 
-const container = document.getElementById("root");
-const root = ReactDOM.createRoot(container);
+// Finds the root of your app
+const container = document.querySelector("#root");
+const root = createRoot(container);
 
+// Tells React to render your app in the root DOM element
 root.render(<App />);
